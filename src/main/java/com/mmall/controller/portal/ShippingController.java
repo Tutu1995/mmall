b0de96af9ba126c773050048a117c9c9e85ca168
Zihao Tao <taozihao@uw.edu>
2018-11-18 16:47:41 -0800

    fix

--- src/main/java/com/mmall/controller/portal/ShippingController.java
+++ src/main/java/com/mmall/controller/portal/ShippingController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import javax.servlet.http.HttpSession;
 
 /**
- * Created by geely
+ * Created by tino on 10/17/18.
  */
 
 @Controller
--- src/main/java/com/mmall/service/impl/ShippingServiceImpl.java
+++ src/main/java/com/mmall/service/impl/ShippingServiceImpl.java
@@ -14,64 +14,60 @@ import java.util.List;
 import java.util.Map;
 
 /**
- * Created by geely
+ * Created by tino on 10/17/18.
  */
+
 @Service("iShippingService")
 public class ShippingServiceImpl implements IShippingService {
-
 
     @Autowired
     private ShippingMapper shippingMapper;
 
+    @Override
     public ServerResponse add(Integer userId, Shipping shipping){
         shipping.setUserId(userId);
         int rowCount = shippingMapper.insert(shipping);
         if(rowCount > 0){
             Map result = Maps.newHashMap();
             result.put("shippingId",shipping.getId());
-            return ServerResponse.createBySuccess("新建地址成功",result);
+            return ServerResponse.createBySuccess("Create address successfully",result);
         }
-        return ServerResponse.createByErrorMessage("新建地址失败");
+        return ServerResponse.createByErrorMessage("Cannot create address");
     }
 
-    public ServerResponse<String> del(Integer userId,Integer shippingId){
-        int resultCount = shippingMapper.deleteByShippingIdUserId(userId,shippingId);
-        if(resultCount > 0){
-            return ServerResponse.createBySuccess("删除地址成功");
+    @Override
+    public ServerResponse<String> del(Integer userId, Integer shippingId) {
+        int resultCount = shippingMapper.deleteByShippingIdUserId(userId, shippingId);
+        if(resultCount > 0) {
+            return ServerResponse.createBySuccess("Delete address successfully");
         }
-        return ServerResponse.createByErrorMessage("删除地址失败");
+        return ServerResponse.createBySuccess("Cannot delete address");
     }
 
-
-    public ServerResponse update(Integer userId, Shipping shipping){
-        shipping.setUserId(userId);
-        int rowCount = shippingMapper.updateByShipping(shipping);
-        if(rowCount > 0){
-            return ServerResponse.createBySuccess("更新地址成功");
+    @Override
+    public ServerResponse<String> update(Integer userId, Shipping shipping) {
+        shipping.setUserId(userId); // must set current userId to shipping to avoid other people use shipping from other users
+        int resultCount = shippingMapper.updateByShipping(shipping);
+        if(resultCount > 0) {
+            return ServerResponse.createBySuccess("Update address successfully");
         }
-        return ServerResponse.createByErrorMessage("更新地址失败");
+        return ServerResponse.createBySuccess("Cannot update address");
     }
 
-    public ServerResponse<Shipping> select(Integer userId, Integer shippingId){
-        Shipping shipping = shippingMapper.selectByShippingIdUserId(userId,shippingId);
-        if(shipping == null){
-            return ServerResponse.createByErrorMessage("无法查询到该地址");
+    @Override
+    public ServerResponse select(Integer userId, Integer shippingId) {
+        Shipping shipping = shippingMapper.selectByShippingIdUserId(userId, shippingId);
+        if(shipping == null) {
+            return ServerResponse.createByErrorMessage("Cannot find the address");
         }
-        return ServerResponse.createBySuccess("更新地址成功",shipping);
+        return ServerResponse.createBySuccess("Find the address successfully", shipping);
     }
 
-
-    public ServerResponse<PageInfo> list(Integer userId,int pageNum,int pageSize){
-        PageHelper.startPage(pageNum,pageSize);
-        List<Shipping> shippingList = shippingMapper.selectByUserId(userId);
-        PageInfo pageInfo = new PageInfo(shippingList);
+    @Override
+    public ServerResponse<PageInfo> list(Integer userId, int pageNum, int pageSize) {
+        PageHelper.startPage(pageNum, pageSize);
+        List<Shipping> result = shippingMapper.selectByUserId(userId);
+        PageInfo pageInfo = new PageInfo(result);
         return ServerResponse.createBySuccess(pageInfo);
     }
-
-
-
-
-
-
-
 }
