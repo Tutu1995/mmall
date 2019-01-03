824dafeeb40cb0e24a2cc2da135f3a1488756284
Zihao Tao <taozihao@uw.edu>
2019-01-02 23:32:34 -0800

    expire

---
+++ src/main/java/com/mmall/controller/common/SessionExpireFilter.java
@@ -0,0 +1,44 @@
+package com.mmall.controller.common;
+
+import com.mmall.common.Const;
+import com.mmall.pojo.User;
+import com.mmall.util.CookieUtil;
+import com.mmall.util.JsonUtil;
+import com.mmall.util.RedisPoolUtil;
+import org.apache.commons.lang.StringUtils;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+/**
+ * Created by tino on 1/2/19.
+ */
+public class SessionExpireFilter implements Filter{
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest httpServletRequest = (HttpServletRequest) request;
+        String loginToken = CookieUtil.readLoginToken(httpServletRequest);
+
+        if(StringUtils.isNotEmpty(loginToken)) {
+            String userJsonStr = RedisPoolUtil.get(loginToken);
+            User user = JsonUtil.String2Obj(userJsonStr, User.class);
+            if(user != null) {
+                // reset expiration
+                RedisPoolUtil.expire(loginToken, Const.RedisCacheEx.REDIS_SESSION_EXTIME);
+            }
+        }
+        chain.doFilter(request, response);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
