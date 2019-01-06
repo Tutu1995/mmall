0fa7799e567752b338e3e9d124ff8d373ffae8f4
Zihao Tao <taozihao@uw.edu>
2019-01-06 00:34:22 -0800

    redis_lock

--- src/main/java/com/mmall/task/CloseOrderTask.java
+++ src/main/java/com/mmall/task/CloseOrderTask.java
@@ -5,8 +5,12 @@ import com.mmall.service.IOrderService;
 import com.mmall.util.PropertiesUtil;
 import com.mmall.util.RedisShardedPoolUtil;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+
+import javax.annotation.PreDestroy;
 
 /**
  * Created by tino on 1/5/19.
@@ -17,6 +21,11 @@ public class CloseOrderTask {
     @Autowired
     private IOrderService iOrderService;
 
+    @PreDestroy
+    public void delLock() {
+        RedisShardedPoolUtil.del(Const.REDIS_LOCK.CLOSE_ORDER_TASK_LOCK);
+    }
+
 //    @Scheduled(cron="0 */1 * * * ?") // per minute
     public void closeOrderTaskV1() {
         log.info("Close order task starts.");
@@ -26,6 +35,7 @@ public class CloseOrderTask {
 
     }
 
+    //@Scheduled(cron="0 */1 * * * ?")
     public void closeOrderTaskV2() {
         log.info("Close order task starts.");
         long lockTimeout = Long.parseLong(PropertiesUtil.getProperty("lock.timeout", "5000"));
@@ -39,11 +49,35 @@ public class CloseOrderTask {
         log.info("Close order task ends.");
     }
 
+    @Scheduled(cron="0 */1 * * * ?")
+    public void closeOrderTaskV3() {
+        log.info("Close order task starts.");
+        long lockTimeout = Long.parseLong(PropertiesUtil.getProperty("lock.timeout", "5000"));
+        Long setnxResult = RedisShardedPoolUtil.setnx(Const.REDIS_LOCK.CLOSE_ORDER_TASK_LOCK, String.valueOf(System.currentTimeMillis() + lockTimeout));
+        if(setnxResult != null && setnxResult == 1) {
+            // if setnxResult = 1, successfully set value, get lock
+            closeOrder(Const.REDIS_LOCK.CLOSE_ORDER_TASK_LOCK);
+        } else {
+            String lockValueStr = RedisShardedPoolUtil.get(Const.REDIS_LOCK.CLOSE_ORDER_TASK_LOCK);
+            if(lockValueStr != null && System.currentTimeMillis() > Long.parseLong(lockValueStr)) {
+                String getSetResult = RedisShardedPoolUtil.getSet(Const.REDIS_LOCK.CLOSE_ORDER_TASK_LOCK, String.valueOf(System.currentTimeMillis() + lockTimeout));
+                if(getSetResult == null || (getSetResult != null && StringUtils.equals(getSetResult, lockValueStr))) {
+                    closeOrder(Const.REDIS_LOCK.CLOSE_ORDER_TASK_LOCK);
+                } else {
+                    log.info("Do not get distributed lock:{}", Const.REDIS_LOCK.CLOSE_ORDER_TASK_LOCK);
+                }
+            } else {
+                log.info("Do not get distributed lock:{}", Const.REDIS_LOCK.CLOSE_ORDER_TASK_LOCK);
+            }
+        }
+        log.info("Close order task ends.");
+    }
+
     private void closeOrder(String lockName) {
-        RedisShardedPoolUtil.expire(lockName, 50);
+        RedisShardedPoolUtil.expire(lockName, 5);
         log.info("Get{}, THreadName:{}", lockName, Thread.currentThread().getName());
         int hour = Integer.parseInt(PropertiesUtil.getProperty("close.order.task.time.hour", "2"));
-        iOrderService.closeOrder(hour);
+        //iOrderService.closeOrder(hour);
         RedisShardedPoolUtil.del(lockName);
         log.info("Release{}, THreadName:{}", lockName, Thread.currentThread().getName());
     }
--- src/main/java/com/mmall/util/RedisShardedPoolUtil.java
+++ src/main/java/com/mmall/util/RedisShardedPoolUtil.java
@@ -108,6 +108,22 @@ public class RedisShardedPoolUtil {
         return result;
     }
 
+    public static String getSet(String key, String value) {
+        ShardedJedis jedis = null;
+        String result = null;
+
+        try {
+            jedis = RedisShardedPool.getJedis();
+            result = jedis.getSet(key, value);
+        } catch (Exception e) {
+            log.error("getset key:{} value:{} error", key, value, e);
+            RedisShardedPool.returnBrokenResource(jedis);
+            return result;
+        }
+        RedisShardedPool.returnResource(jedis);
+        return result;
+    }
+
     public static void main(String[] args) {
         ShardedJedis jedis = RedisShardedPool.getJedis();
         RedisShardedPoolUtil.set("KeyTest", "value");
