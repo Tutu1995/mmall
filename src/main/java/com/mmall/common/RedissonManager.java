d45ce6c410ac59296b7c6d51ce8d4d88042b5a9d
Zihao Tao <taozihao@uw.edu>
2019-01-06 15:58:39 -0800

    redisson

---
+++ src/main/java/com/mmall/common/RedissonManager.java
@@ -0,0 +1,45 @@
+package com.mmall.common;
+
+import com.mmall.util.PropertiesUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.redisson.Redisson;
+import org.redisson.config.Config;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+
+/**
+ * Created by tino on 1/6/19.
+ */
+@Component
+@Slf4j
+public class RedissonManager {
+
+    private Config config = new Config();
+
+    private Redisson redisson = null;
+
+    public Redisson getRedisson() {
+        return redisson;
+    }
+
+    private static String redis1Ip = PropertiesUtil.getProperty("redis1.ip");
+    private static Integer redis1Port = Integer.parseInt(PropertiesUtil.getProperty("redis1.port"));
+
+    private static String redis2Ip = PropertiesUtil.getProperty("redis2.ip");
+    private static Integer redis2Port = Integer.parseInt(PropertiesUtil.getProperty("redis2.port"));
+
+    @PostConstruct
+    private void init() {
+        try {
+            config.useSingleServer().setAddress(new StringBuilder().append(redis1Ip).append(":").append(redis1Port).toString());
+            redisson = (Redisson) Redisson.create(config);
+
+            log.info("Initialization ends");
+        } catch (Exception e) {
+            log.error("Redisson init error:", e);
+        }
+    }
+
+
+}
--- src/main/java/com/mmall/task/CloseOrderTask.java
+++ src/main/java/com/mmall/task/CloseOrderTask.java
@@ -1,16 +1,19 @@
 package com.mmall.task;
 
 import com.mmall.common.Const;
+import com.mmall.common.RedissonManager;
 import com.mmall.service.IOrderService;
 import com.mmall.util.PropertiesUtil;
 import com.mmall.util.RedisShardedPoolUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang.StringUtils;
+import org.redisson.api.RLock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PreDestroy;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by tino on 1/5/19.
@@ -20,6 +23,9 @@ import javax.annotation.PreDestroy;
 public class CloseOrderTask {
     @Autowired
     private IOrderService iOrderService;
+
+    @Autowired
+    private RedissonManager redissonManager;
 
     @PreDestroy
     public void delLock() {
@@ -49,7 +55,7 @@ public class CloseOrderTask {
         log.info("Close order task ends.");
     }
 
-    @Scheduled(cron="0 */1 * * * ?")
+    //@Scheduled(cron="0 */1 * * * ?")
     public void closeOrderTaskV3() {
         log.info("Close order task starts.");
         long lockTimeout = Long.parseLong(PropertiesUtil.getProperty("lock.timeout", "5000"));
@@ -73,11 +79,35 @@ public class CloseOrderTask {
         log.info("Close order task ends.");
     }
 
+    @Scheduled(cron="0 */1 * * * ?")
+    public void closeOrderTaskV4() {
+        RLock lock = redissonManager.getRedisson().getLock(Const.REDIS_LOCK.CLOSE_ORDER_TASK_LOCK);
+        boolean getLock = false;
+        try {
+            if(getLock = lock.tryLock(2, 5, TimeUnit.SECONDS)) {
+                log.info("Redisson get distributed lock:{}, ThreadName:{}", Const.REDIS_LOCK.CLOSE_ORDER_TASK_LOCK, Thread.currentThread().getName());
+                int hour = Integer.parseInt(PropertiesUtil.getProperty("close.order.task.time.hour", "2"));
+                //iOrderService.closeOrder(hour);
+            } else {
+                log.info("Redisson does not get distributed lock:{}, ThreadName:{}", Const.REDIS_LOCK.CLOSE_ORDER_TASK_LOCK, Thread.currentThread().getName());
+            }
+        } catch (InterruptedException e) {
+            log.error("Redisson get distributed lock error:", e);
+        } finally {
+            if(!getLock) {
+                return;
+            }
+            lock.unlock();
+            log.info("Redisson release lock.");
+
+        }
+    }
+
     private void closeOrder(String lockName) {
         RedisShardedPoolUtil.expire(lockName, 5);
         log.info("Get{}, THreadName:{}", lockName, Thread.currentThread().getName());
         int hour = Integer.parseInt(PropertiesUtil.getProperty("close.order.task.time.hour", "2"));
-        //iOrderService.closeOrder(hour);
+        iOrderService.closeOrder(hour);
         RedisShardedPoolUtil.del(lockName);
         log.info("Release{}, THreadName:{}", lockName, Thread.currentThread().getName());
     }
